f35ae54a51829de1cbeea1e9125b451759c8ad11
ANTONIO  NETO <snetinho40@gmail.com>
2025-06-04 18:39:27 -0300

    up

---
+++ Js2/saladas.js
@@ -0,0 +1,186 @@
+export const receitasSaladas = {  
+  saladaFrutas: {
+    titulo: 'Salada de Frutas Tropical',
+    descricao: 'Mistura refrescante de frutas tropicais com um toque cítrico.',
+    imagem: 'imagens/salada-frutas.jpg',
+    ingredientes: [
+      '1 manga cortada em cubos',
+      '1 abacaxi cortado em cubos',
+      '2 kiwis fatiados',
+      '1 maçã picada',
+      'Suco de 1 limão',
+      'Mel a gosto',
+      'Folhas de hortelã para decorar'
+    ],
+    preparo: [
+      'Misture todas as frutas em uma tigela.',
+      'Regue com o suco de limão e o mel.',
+      'Misture delicadamente e decore com folhas de hortelã.',
+      'Sirva gelado.'
+    ]
+  },
+
+  saladaCaesar: {
+    titulo: 'Salada Caesar',
+    descricao: 'Clássica salada com alface, croutons e molho especial Caesar.',
+    imagem: 'imagens/salada-caesar.jpg',
+    ingredientes: [
+      '1 pé de alface romana',
+      '100g de croutons',
+      '50g de queijo parmesão ralado',
+      'Molho Caesar a gosto'
+    ],
+    preparo: [
+      'Lave e rasgue a alface.',
+      'Misture a alface com os croutons.',
+      'Adicione o molho Caesar e o queijo parmesão.',
+      'Misture bem e sirva.'
+    ]
+  },
+
+  saladaGrega: {
+    titulo: 'Salada Grega',
+    descricao: 'Salada fresca com tomates, pepino, cebola, azeitonas e queijo feta.',
+    imagem: 'imagens/salada-grega.jpg',
+    ingredientes: [
+      '2 tomates cortados em cubos',
+      '1 pepino fatiado',
+      '1 cebola roxa fatiada',
+      '100g de queijo feta',
+      'Azeitonas pretas a gosto',
+      'Orégano, azeite e suco de limão a gosto'
+    ],
+    preparo: [
+      'Misture todos os ingredientes em uma tigela.',
+      'Tempere com orégano, azeite e suco de limão.',
+      'Sirva fresca.'
+    ]
+  },
+
+  saladaCaprese: {
+    titulo: 'Salada Caprese',
+    descricao: 'Salada italiana simples com tomate, mussarela e manjericão.',
+    imagem: 'imagens/salada-caprese.jpg',
+    ingredientes: [
+      'Tomates fatiados',
+      'Mussarela de búfala fatiada',
+      'Folhas de manjericão fresco',
+      'Azeite, sal e pimenta a gosto'
+    ],
+    preparo: [
+      'Alterne fatias de tomate e mussarela em um prato.',
+      'Decore com folhas de manjericão.',
+      'Regue com azeite e tempere com sal e pimenta.',
+      'Sirva imediatamente.'
+    ]
+  },
+
+  saladaRussa: {
+    titulo: 'Salada Russa',
+    descricao: 'Salada clássica com legumes, maionese e ervilhas.',
+    imagem: 'imagens/salada-russa.jpg',
+    ingredientes: [
+      '2 batatas cozidas em cubos',
+      '2 cenouras cozidas em cubos',
+      '1 xícara de ervilhas cozidas',
+      '1 maçã picada',
+      '1 xícara de maionese',
+      'Sal e pimenta a gosto'
+    ],
+    preparo: [
+      'Misture todos os legumes e a maçã em uma tigela.',
+      'Adicione a maionese e tempere.',
+      'Misture bem e sirva gelada.'
+    ]
+  },
+
+  saladaQuinoa: {
+    titulo: 'Salada de Quinoa com Legumes',
+    descricao: 'Salada nutritiva com quinoa, legumes frescos e molho cítrico.',
+    imagem: 'imagens/salada-quinoa.jpg',
+    ingredientes: [
+      '1 xícara de quinoa cozida',
+      '1 pepino picado',
+      '1 tomate picado',
+      '1/2 cebola picada',
+      'Salsinha e hortelã a gosto',
+      'Suco de 1 limão',
+      'Azeite, sal e pimenta a gosto'
+    ],
+    preparo: [
+      'Misture todos os ingredientes em uma tigela.',
+      'Tempere com suco de limão, azeite, sal e pimenta.',
+      'Sirva fria ou em temperatura ambiente.'
+    ]
+  },
+
+  saladaAlfaceTomate: {
+    titulo: 'Salada Simples de Alface e Tomate',
+    descricao: 'Salada fresca e leve para acompanhar qualquer prato.',
+    imagem: 'imagens/salada-alface-tomate.jpg',
+    ingredientes: [
+      '1 pé de alface',
+      '2 tomates fatiados',
+      '1 cebola fatiada',
+      'Azeite, vinagre, sal e pimenta a gosto'
+    ],
+    preparo: [
+      'Lave e rasgue a alface.',
+      'Misture com os tomates e a cebola.',
+      'Tempere com azeite, vinagre, sal e pimenta.',
+      'Sirva fresca.'
+    ]
+  },
+
+  saladaPalmito: {
+    titulo: 'Salada de Palmito com Tomate',
+    descricao: 'Salada saborosa com palmito, tomate e cebola.',
+    imagem: 'imagens/salada-palmito.jpg',
+    ingredientes: [
+      '1 vidro de palmito picado',
+      '2 tomates picados',
+      '1 cebola picada',
+      'Salsinha, azeite, sal e pimenta a gosto'
+    ],
+    preparo: [
+      'Misture todos os ingredientes em uma tigela.',
+      'Tempere a gosto.',
+      'Sirva fresca.'
+    ]
+  },
+
+  saladaCenouraBeterraba: {
+    titulo: 'Salada de Cenoura e Beterraba',
+    descricao: 'Salada colorida e nutritiva com cenoura e beterraba raladas.',
+    imagem: 'imagens/salada-cenoura-beterraba.jpg',
+    ingredientes: [
+      '2 cenouras raladas',
+      '2 beterrabas raladas',
+      'Suco de 1 limão',
+      'Azeite, sal e pimenta a gosto'
+    ],
+    preparo: [
+      'Misture a cenoura e a beterraba raladas.',
+      'Tempere com suco de limão, azeite, sal e pimenta.',
+      'Sirva fresca.'
+    ]
+  },
+
+  saladaAtum: {
+    titulo: 'Salada de Atum',
+    descricao: 'Salada rápida com atum, alface e legumes variados.',
+    imagem: 'imagens/salada-atum.jpg',
+    ingredientes: [
+      '1 lata de atum',
+      '1 pé de alface',
+      '1 tomate picado',
+      '1/2 cebola picada',
+      'Azeite, sal e pimenta a gosto'
+    ],
+    preparo: [
+      'Misture todos os ingredientes em uma tigela.',
+      'Tempere a gosto.',
+      'Sirva fresca.'
+    ]
+  }
+};

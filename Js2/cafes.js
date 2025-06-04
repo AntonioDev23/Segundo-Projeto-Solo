b9d47115698fd5767bade58efce33e09dc99ac86
ANTONIO  NETO <snetinho40@gmail.com>
2025-06-04 20:20:42 -0300

    up

---
+++ Js2/cafes.js
@@ -0,0 +1,158 @@
+export const cafeDaManha = [
+  {
+    titulo: 'Pão na Chapa',
+    imagem: 'https://www.receiteria.com.br/wp-content/uploads/pao-na-chapa-730x548.jpeg',
+    ingredientes: [
+      '2 fatias de pão francês',
+      'Manteiga a gosto',
+      'Café ou suco para acompanhar'
+    ],
+    preparo: [
+      'Passe manteiga nas fatias de pão.',
+      'Coloque na chapa quente até dourar.',
+      'Sirva quente com café ou suco.'
+    ]
+  },
+  {
+    titulo: 'Tapioca com Queijo',
+    imagem: 'https://www.receiteria.com.br/wp-content/uploads/tapioca-recheada-com-queijo-e-presunto-730x548.jpeg',
+    ingredientes: [
+      '3 colheres de sopa de goma de tapioca',
+      '1 fatia de queijo mussarela',
+      'Sal a gosto'
+    ],
+    preparo: [
+      'Aqueça a frigideira e espalhe a goma de tapioca.',
+      'Quando firmar, vire e adicione o queijo.',
+      'Dobre ao meio e sirva quente.'
+    ]
+  },
+  {
+    titulo: 'Ovos Mexidos',
+    imagem: 'https://www.receiteria.com.br/wp-content/uploads/ovos-mexidos-730x548.jpeg',
+    ingredientes: [
+      '3 ovos',
+      'Sal e pimenta a gosto',
+      'Manteiga para untar'
+    ],
+    preparo: [
+      'Bata os ovos com sal e pimenta.',
+      'Aqueça a manteiga na frigideira.',
+      'Despeje os ovos e mexa até ficarem cremosos.',
+      'Sirva quente.'
+    ]
+  },
+  {
+    titulo: 'Vitamina de Frutas',
+    imagem: 'https://www.receiteria.com.br/wp-content/uploads/vitamina-de-frutas-730x548.jpeg',
+    ingredientes: [
+      '1 banana',
+      '1/2 mamão',
+      '1 copo de leite',
+      'Mel a gosto'
+    ],
+    preparo: [
+      'Bata todos os ingredientes no liquidificador.',
+      'Sirva gelado.'
+    ]
+  },
+  {
+    titulo: 'Panqueca Simples',
+    imagem: 'https://www.receiteria.com.br/wp-content/uploads/panqueca-simples-730x548.jpeg',
+    ingredientes: [
+      '1 xícara de farinha de trigo',
+      '1 ovo',
+      '1 xícara de leite',
+      '1 colher de sopa de açúcar',
+      'Manteiga para untar'
+    ],
+    preparo: [
+      'Misture todos os ingredientes até formar uma massa homogênea.',
+      'Unte a frigideira e despeje a massa.',
+      'Cozinhe dos dois lados até dourar.',
+      'Sirva com mel ou geleia.'
+    ]
+  },
+  {
+    titulo: 'Cuscuz Nordestino',
+    imagem: 'https://www.receiteria.com.br/wp-content/uploads/cuscuz-nordestino-730x548.jpeg',
+    ingredientes: [
+      '1 xícara de farinha de milho para cuscuz',
+      'Sal a gosto',
+      'Manteiga para servir'
+    ],
+    preparo: [
+      'Coloque a farinha em uma cuscuzeira.',
+      'Cozinhe no vapor por 15 minutos.',
+      'Sirva quente com manteiga.'
+    ]
+  },
+  {
+    titulo: 'Mingau de Aveia',
+    imagem: 'https://www.receiteria.com.br/wp-content/uploads/mingau-de-aveia-730x548.jpeg',
+    ingredientes: [
+      '1/2 xícara de aveia em flocos',
+      '2 xícaras de leite',
+      'Mel ou açúcar a gosto'
+    ],
+    preparo: [
+      'Misture a aveia e o leite em uma panela.',
+      'Cozinhe em fogo médio até engrossar.',
+      'Adoce a gosto e sirva quente.'
+    ]
+  },
+  {
+    titulo: 'Pão de Queijo',
+    imagem: 'https://www.receiteria.com.br/wp-content/uploads/pao-de-queijo-730x548.jpeg',
+    ingredientes: [
+      '500g de polvilho doce',
+      '200ml de leite',
+      '100ml de óleo',
+      '2 ovos',
+      '200g de queijo minas ralado',
+      'Sal a gosto'
+    ],
+    preparo: [
+      'Ferva o leite com o óleo e o sal.',
+      'Misture com o polvilho e espere amornar.',
+      'Adicione ovos e queijo, misture bem.',
+      'Forme bolinhas e asse em forno médio por 25 minutos.'
+    ]
+  },
+  {
+    titulo: 'Café com Leite',
+    imagem: 'https://www.receiteria.com.br/wp-content/uploads/cafe-com-leite-730x548.jpeg',
+    ingredientes: [
+      '1 xícara de café forte',
+      '1 xícara de leite quente',
+      'Açúcar a gosto'
+    ],
+    preparo: [
+      'Prepare o café forte.',
+      'Aqueça o leite separadamente.',
+      'Misture e adoce a gosto.',
+      'Sirva quente.'
+    ]
+  },
+  {
+    titulo: 'Bolo de Fubá',
+    imagem: 'https://www.receiteria.com.br/wp-content/uploads/bolo-de-fuba-730x548.jpeg',
+    ingredientes: [
+      '1 xícara de fubá',
+      '1 xícara de farinha de trigo',
+      '1 xícara de açúcar',
+      '3 ovos',
+      '1/2 xícara de óleo',
+      '1 xícara de leite',
+      '1 colher de sopa de fermento em pó'
+    ],
+    preparo: [
+      'Misture todos os ingredientes, exceto o fermento.',
+      'Adicione o fermento por último e misture delicadamente.',
+      'Despeje em forma untada.',
+      'Asse em forno médio por 40 minutos.'
+    ]
+  }
+];
+
+

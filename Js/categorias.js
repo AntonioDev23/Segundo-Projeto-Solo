031c2523c263e003633db31c1bd2cff10cbf86aa
ANTONIO  NETO <snetinho40@gmail.com>
2025-06-05 20:34:36 -0300

    up

--- Js/categorias.js
+++ Js/categorias.js
@@ -124,10 +124,11 @@ document.addEventListener('DOMContentLoaded', () => {
             bebidasModule.mostrarBebidas();
           }
 
-          if (cat.nome.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '') === 'cafes') {
+          if (cat.nome.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '') === 'cafe da manha') {
             const { mostrarCafes } = await import('../js2/cafes.js');
             mostrarCafes();
           }
+
 
 
           

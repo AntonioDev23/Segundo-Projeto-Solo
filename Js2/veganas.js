200e54b37aa1435ab4b91fa36c24d33a8374d4f0
ANTONIO  NETO <snetinho40@gmail.com>
2025-06-05 20:54:00 -0300

    up

---
+++ Js2/veganas.js
@@ -0,0 +1,184 @@
+export const receitasVeganas = [
+  {
+    titulo: 'Estrogonofe de Grão-de-Bico',
+    imagem: '',
+    ingredientes: [
+      '2 xícaras de grão-de-bico cozido',
+      '1 cebola picada',
+      '2 dentes de alho picados',
+      '1/2 xícara de molho de tomate',
+      '1/2 xícara de leite de coco',
+      'Sal e pimenta a gosto',
+      'Azeite para refogar'
+    ],
+    preparo: [
+      'Refogue a cebola e o alho no azeite.',
+      'Adicione o grão-de-bico e misture bem.',
+      'Coloque o molho de tomate e o leite de coco.',
+      'Tempere com sal e pimenta.',
+      'Cozinhe por 5 a 10 minutos e sirva.'
+    ]
+  },
+  {
+    titulo: 'Lasanha de Berinjela',
+    imagem: '',
+    ingredientes: [
+      '2 berinjelas cortadas em fatias finas',
+      '1 xícara de molho de tomate',
+      '1 xícara de tofu amassado com temperos',
+      'Orégano a gosto',
+      'Azeite para grelhar'
+    ],
+    preparo: [
+      'Grelhe as fatias de berinjela em uma frigideira com azeite.',
+      'Monte a lasanha em camadas: molho, berinjela, tofu.',
+      'Finalize com molho e orégano.',
+      'Leve ao forno por 20 minutos e sirva.'
+    ]
+  },
+  {
+    titulo: 'Moqueca Vegana',
+    imagem: '',
+    ingredientes: [
+      '1 pimentão vermelho fatiado',
+      '1 pimentão amarelo fatiado',
+      '1 tomate picado',
+      '1 cebola fatiada',
+      '200ml de leite de coco',
+      '2 colheres de sopa de azeite de dendê',
+      'Coentro a gosto',
+      'Sal e pimenta'
+    ],
+    preparo: [
+      'Refogue os vegetais no azeite de dendê.',
+      'Adicione o leite de coco e cozinhe por 15 minutos.',
+      'Tempere com sal, pimenta e coentro.',
+      'Sirva com arroz branco.'
+    ]
+  },
+  {
+    titulo: 'Falafel Assado',
+    imagem: '',
+    ingredientes: [
+      '2 xícaras de grão-de-bico cru (de molho por 12h)',
+      '1/2 cebola picada',
+      '2 dentes de alho',
+      '1/2 xícara de salsinha',
+      '1 colher de chá de cominho',
+      'Sal e pimenta a gosto'
+    ],
+    preparo: [
+      'Processe todos os ingredientes até formar uma massa.',
+      'Modele bolinhas e coloque em uma assadeira.',
+      'Asse por 25 minutos a 200°C, virando na metade.',
+      'Sirva com molho tahine.'
+    ]
+  },
+  {
+    titulo: 'Escondidinho de Lentilha',
+    imagem: '',
+    ingredientes: [
+      '2 xícaras de lentilha cozida',
+      '1 cebola picada',
+      '1 dente de alho',
+      '3 batatas cozidas e amassadas',
+      'Sal e pimenta a gosto',
+      'Azeite para refogar'
+    ],
+    preparo: [
+      'Refogue a cebola e o alho, adicione a lentilha e tempere.',
+      'Coloque a lentilha num refratário e cubra com o purê de batata.',
+      'Leve ao forno para gratinar por 20 minutos.',
+      'Sirva quente.'
+    ]
+  },
+  {
+    titulo: 'Hambúrguer de Lentilha',
+    imagem: '',
+    ingredientes: [
+      '1 xícara de lentilha cozida',
+      '1/2 cebola ralada',
+      '2 colheres de sopa de farinha de aveia',
+      '1 colher de chá de páprica',
+      'Sal e pimenta a gosto'
+    ],
+    preparo: [
+      'Amasse a lentilha e misture os demais ingredientes.',
+      'Modele hambúrgueres e grelhe em frigideira antiaderente.',
+      'Doure dos dois lados e sirva com pão ou salada.'
+    ]
+  },
+  {
+    titulo: 'Macarrão com Molho de Castanha',
+    imagem: '',
+    ingredientes: [
+      '250g de macarrão sem ovos',
+      '1/2 xícara de castanhas de caju (de molho por 4h)',
+      '1 dente de alho',
+      '1 colher de sopa de azeite',
+      '1/2 xícara de água',
+      'Sal e noz-moscada a gosto'
+    ],
+    preparo: [
+      'Bata as castanhas com água, alho, azeite e temperos até virar um creme.',
+      'Cozinhe o macarrão e escorra.',
+      'Misture com o molho e sirva quente.'
+    ]
+  },
+  {
+    titulo: 'Arroz de Couve-Flor',
+    imagem: '',
+    ingredientes: [
+      '1 cabeça de couve-flor',
+      '1 dente de alho picado',
+      '1 colher de sopa de azeite',
+      'Sal a gosto',
+      'Cheiro-verde para finalizar'
+    ],
+    preparo: [
+      'Rale ou pulse a couve-flor no processador até ficar como grãos de arroz.',
+      'Refogue o alho no azeite e adicione a couve-flor.',
+      'Cozinhe por 5 minutos, mexendo sempre.',
+      'Finalize com cheiro-verde e sirva.'
+    ]
+  },
+  {
+    titulo: 'Curry de Legumes',
+    imagem: '',
+    ingredientes: [
+      '1 batata picada',
+      '1 cenoura em rodelas',
+      '1 xícara de ervilhas',
+      '1/2 cebola picada',
+      '200ml de leite de coco',
+      '1 colher de sopa de curry em pó',
+      'Sal a gosto'
+    ],
+    preparo: [
+      'Refogue a cebola, adicione os legumes e o curry.',
+      'Coloque o leite de coco e cozinhe até os legumes ficarem macios.',
+      'Acerte o sal e sirva com arroz.'
+    ]
+  },
+  {
+    titulo: 'Torta de Legumes com Massa de Grão-de-Bico',
+    imagem: '',
+    ingredientes: [
+      '1 xícara de farinha de grão-de-bico',
+      '1/2 xícara de água',
+      '2 colheres de sopa de azeite',
+      '1 cenoura ralada',
+      '1/2 abobrinha picada',
+      '1/2 cebola picada',
+      'Sal, pimenta e orégano a gosto'
+    ],
+    preparo: [
+      'Misture a farinha com água, azeite e temperos.',
+      'Adicione os legumes e mexa bem.',
+      'Despeje em uma forma untada e leve ao forno por 30 minutos.',
+      'Sirva morna ou fria.'
+    ]
+  }
+];
+
+
